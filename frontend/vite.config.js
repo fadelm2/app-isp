7b9fdf22a81c2d446c5623e15d57c69a8a145bdb
Fadel Muhammad <fadelmuhammad128@gmail.com>
2026-07-16 09:10:27 +0700

    push

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const backendUrl = process.env.VITE_BACKEND_URL || 'http://localhost:9030'
+const backendUrl = process.env.VITE_BACKEND_URL || 'http://172.16.23.70:9030'
 
 // https://vitejs.dev/config/
 export default defineConfig({
